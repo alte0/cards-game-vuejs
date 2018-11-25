63cb3fb2bce418654c6315b92ec089e5faab5ff8
m.dmitriev <makc-dmv@yandex.ru>
2018-11-25 15:03:33 +0500

    игровое состояние заменено на игровой обьект

--- src/store.js
+++ src/store.js
@@ -6,21 +6,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    points: 0,
-    CARDS_ARRAY_VALUE: ['0', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'A', 'K', 'Q'],
-    CARDS_ARRAY_SUITS: ['C', 'D', 'H', 'S'],
-    cards: []
+    STATE_GAME: {
+      POINTS: 0,
+      CARDS_ARRAY_VALUE: ['0', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'A', 'K', 'Q'],
+      CARDS_ARRAY_SUITS: ['C', 'D', 'H', 'S'],
+      CARDS: []
+    }
   },
   mutations: {
     setPoints (state, payload) {
-      state.points = payload
+      state.STATE_GAME.POINTS = payload
     },
     randomCards (state) {
-      state.cards = []
+      state.STATE_GAME.CARDS = []
       let cardsTemp = []
 
-      let copyValueCards = state.CARDS_ARRAY_VALUE.slice()
-      const randomSuitCard = state.CARDS_ARRAY_SUITS[returnRandomValueArray(state.CARDS_ARRAY_SUITS)]
+      let copyValueCards = state.STATE_GAME.CARDS_ARRAY_VALUE.slice()
+      const randomSuitCard = state.STATE_GAME.CARDS_ARRAY_SUITS[returnRandomValueArray(state.STATE_GAME.CARDS_ARRAY_SUITS)]
 
       for (let i = 0; i < 9; i++) {
         let valueCard = copyValueCards.splice(returnRandomValueArray(copyValueCards), 1)[0]
@@ -30,7 +32,7 @@ export default new Vuex.Store({
       }
 
       for (let i = 0; i < 18; i++) {
-        state.cards.push(cardsTemp.splice(returnRandomValueArray(cardsTemp), 1)[0])
+        state.STATE_GAME.CARDS.push(cardsTemp.splice(returnRandomValueArray(cardsTemp), 1)[0])
       }
     }
   },
@@ -38,10 +40,10 @@ export default new Vuex.Store({
   },
   getters: {
     getPoints (state) {
-      return state.points
+      return state.STATE_GAME.POINTS
     },
     getCards (state) {
-      return state.cards
+      return state.STATE_GAME.CARDS
     }
   }
 })
